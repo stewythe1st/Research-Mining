fb35eca0a318256142c6d64bbd3a7a2e1b1feefb
Miller, Stuart (S&T-Student) <sm67c@mst.edu>
2018-03-04 19:20:34 -0600

    Added basic code for LDA.

--- getFromGScholar.m
+++ getFromGScholar.m
@@ -13,14 +13,14 @@ page = 0;
 if ~exist('pdfs', 'dir')
     mkdir('pdfs');
 end
-dir = join(['pdfs/',searchterms],'');
-if ~exist(dir, 'dir')
-     mkdir(dir);
+directory = join(['pdfs/',searchterms],'');
+if ~exist(directory, 'dir')
+     mkdir(directory);
 end
 
-% Cookie stuff
-cookie1 = matlab.net.http.Cookie('GSP','A=6qKj5w:CPTS=1520124685:LM=1520124685:S=buUR1ahVyKEgMR2G');
-cookie2 = matlab.net.http.Cookie('NID','125=iSO-4VkA3rF1B_zLnWokM6p-H-SZgqOG4v-seYM8dHVkyi3r4LUbLdj6MYCM6k6gTJ2gS3-UEvSpvMB_9hSiJGWkmtm7k68v_xuORfxysy8UlUpnMnDcSZBG0RWdGFCZDh3k7mCTPa32F7D1YVPLcwvDchGA5I-97WqjjTtDMKjTfstnx9HHt_o1lNfo_mJoUuGfS8Jsviz0JehiGYFZlCIOqQLTjKQuaqPoBTKepFbsIFepoyDmt_jP89tv5MyYgZTwQXZ1kbLBzT3ELfM');
+% Cookie stuffz
+cookie1 = matlab.net.http.Cookie('GSP','IN=7e6cc990821af63+d8a86af7aa9cc61b:LD=en:A=6qKj5w:CPTS=1520203338:LM=1520203338:S=hVWpKlmnVkesE2ii');
+cookie2 = matlab.net.http.Cookie('NID','125=MtLp4THhyZfUWshnOHI2GNHuTBys_a-egHn3ypLVPZ5I0CtkxfeMdMqNBMSLrD8GfJvPJOjXn_G_D3tVhclVgTAEh1mgH6Do1pB1yKgkUmpU57JcaA57JsloaqWhhOhL1GLg9XCAwT33Fqxf2f3JRApjEd8RWkK7SobClpFfy8jgB94RWPsVTOJGwWQMoBM1iBhyjlQXg0dUgwyFlVtNtCoBx-e3h4eS8ixzArKI787V3T9GvororcZYYDIHY2Ymd-qa8jgstpQ-aFtezVQ');
 request = matlab.net.http.RequestMessage;
 request = request.addFields(matlab.net.http.field.CookieField([cookie1 cookie2]));
 
@@ -44,7 +44,8 @@ while k <= articles && page <= maxPages
             j = j - 1;
         end
         pdfs(k,1) = string(searchText(j+1:i+3));
-        pdfs(k,1) = join([dir,'/',pdfs(k,1)],'');
+        pdfs(k,1) = regexprep(pdfs(k,1),':|&|?|=|?|;','');
+        pdfs(k,1) = join([directory,'/',pdfs(k,1)],'');
         
         % Check to see if we've already saved a PDF by this name
         if any(strcmp(pdfs(k,1),pdfs(1:k-1,1)))
@@ -60,6 +61,11 @@ while k <= articles && page <= maxPages
         % Try downloading the PDF
         try
             websave(pdfs(k,1), pdfs(k,2));
+            temp = dir([char(pdfs(k,1)),'*']);
+            temp = temp.name;
+            if(endsWith(temp,'.html'))
+                throw MException('','Downloaded file is not a PDF');
+            end
             
         % If download failed, show a warning and delete the invalid file
         catch ME
--- test.m
+++ test.m
@@ -5,23 +5,46 @@ query = 'k+means';
 
 % Setup
 folder = dir(['pdfs/',query,'/*.pdf']);
-rawDocuments(1:size(folder,1)) = tokenizedDocument;
-procDocuments(1:size(folder,1)) = tokenizedDocument;
+rawDocuments(1:numel(folder)) = tokenizedDocument;
+rawDocuments = rawDocuments';
+procDocuments(1:numel(folder)) = tokenizedDocument;
+procDocuments = procDocuments';
 
 % Read in text from each PDF and preprocess it
-for i=1:size(folder,1)
+parfor i=1:numel(folder)
     text = extractFileText([folder(i).folder,'\',folder(i).name]);
     procDocuments(i) = preprocess(text);
     rawDocuments(i) = tokenizedDocument(text);
 end
+poolobj = gcp('nocreate');
+delete(poolobj);
 
 % Make into a bag of words
 rawBag = bagOfWords(rawDocuments);
 cleanBag = bagOfWords(procDocuments);
 cleanBag = removeInfrequentWords(cleanBag,2);
 [cleanBag,idx] = removeEmptyDocuments(cleanBag);
-reduction = 1 - cleanBag.NumWords/rawBag.NumWords
+reduction = 1 - cleanBag.NumWords/rawBag.NumWords;
 
 % Show a word cloud, just for fun
+% figure
+% wordcloud(cleanBag);
+
+numTopicsRange = [5 10 15 20 25];
+ppl(1:numel(numTopicsRange)) = double(0);
+parfor i = 1:numel(numTopicsRange)
+    numTopics = numTopicsRange(i);
+    mdl(i) = fitlda(cleanBag,numTopics,'Verbose',0);
+    [~,ppl(i)] = logp(mdl(i),procDocuments);
+end
+poolobj = gcp('nocreate');
+delete(poolobj);
+
+
 figure
-wordcloud(cleanBag);
+topicMixtures = transform(mdl(4),procDocuments(1:10));
+barh(topicMixtures(1:10,:),'stacked')
+xlim([0 1])
+title("Topic Mixtures")
+xlabel("Topic Probability")
+ylabel("Document")
